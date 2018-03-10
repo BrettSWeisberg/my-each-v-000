adfdd85afb3226cdfcca204916705e7cf4e0f564
Brett Weisberg <weisberg.brett@gmail.com>
2018-03-10 22:02:46 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -4,8 +4,8 @@ def my_each(array)
   new_array = []
   counter = 0
   while counter < array.length
-    yield
     new_array << array[counter]
+    yield
     return new_array
     counter += 1
   end
